d1db7e922211eef9d7851616817a40c5fa44d41c
Guilherme Silva <git@github.com:guilhermegalvaosilva/news-explorer-frontend.git>
2025-12-16 12:19:22 +0000

    minha empresa de 3d

---
+++ script.js
@@ -0,0 +1,14 @@
+const sections = document.querySelectorAll(".fade-section");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+sections.forEach((section) => observer.observe(section));
